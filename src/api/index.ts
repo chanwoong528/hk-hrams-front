c0494ef66884b23e561108af12e927605d7af303
mooncw@hankookilbo.com <mooncw@hankookilbo.com>
2026-03-04 15:35:12 +0900

    fix: api route

--- src/api/index.ts
+++ src/api/index.ts
@@ -8,7 +8,7 @@ import setupTokenRefresh from "xior/plugins/token-refresh";
 
 export const http = xior.create({
   baseURL: import.meta.env.PROD
-    ? "http://hk-hrams-alb-283672731.ap-northeast-2.elb.amazonaws.com"
+    ? "https://hr-api-dev.hankookilbo.com"
     : "http://localhost:3000",
   headers: {
     "Content-Type": "application/json",
